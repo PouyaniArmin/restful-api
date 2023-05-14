c80c1cf2d1b159d0bc6b2ee9d315eb7b06b52cb3
PouyaniArmin <pouyaniarmin@gmail.com>
2023-05-14 18:12:44 +0330

    index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ use Core\Application;
 require "./vendor/autoload.php";
 
 $app=new Application();
-$app->router->get('/','home');
+$app->router->get('/',[HomeControllers::class,'index']);
 $app->router->get('/contact',[ContactUsControllers::class,'index']);
 $app->run();
 
